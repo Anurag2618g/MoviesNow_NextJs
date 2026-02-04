f55c60d8c44085edadec4812e1fb22219504b008
Anurag2618g <anurag2618g@gmail.com>
2026-02-04 23:56:48 +0530

    update watch history

--- server/watch/watch.history.query.ts
+++ server/watch/watch.history.query.ts
@@ -1,49 +1,29 @@
-import { connectDB } from "../db/mongo";
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import WatchHistory from "./watch.model";
 
-type Cursor = {
-    lastWatchedAt: string,
-    id: string
-};
+export async function getWatchHistoryPage({
+  userId,
+  limit,
+  cursor,
+}: {
+  userId: string;
+  limit: number;
+  cursor?: { lastWatchedAt: string; id: string };
+}) {
+  const query: any = { userId };
 
-type HistoryQuery = {
-    userId: string,
-    limit: number,
-    cursor?: Cursor,
-};
+  if (cursor) {
+    query.$or = [
+      { lastWatchedAt: { $lt: new Date(cursor.lastWatchedAt) } },
+      {
+        lastWatchedAt: new Date(cursor.lastWatchedAt),
+        _id: { $lt: cursor.id },
+      },
+    ];
+  }
 
-export const getWatchHistory = async({ userId, limit, cursor }: HistoryQuery) => {
-    await connectDB();
-    
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const query: any = { userId };
-    
-    if (cursor) {
-        query.$or = [
-            { lastWatchedAt: { $lt: new Date(cursor.lastWatchedAt) } },
-            { 
-                lastWatchedAt: new Date(cursor.lastWatchedAt),
-                _id: { $lt: cursor.id}
-            }
-        ];
-    }
-
-    const items = await WatchHistory.find(query)
-        .sort({ lastWatchedAt: -1, _id: -1 })
-        .limit(limit)
-        .select({
-            contentId: 1,
-            progress: 1,
-            duration: 1,
-            status: 1,
-            lastWatchedAt: 1,
-        })
-        .lean();
-    
-    const nextCursor = items.length === limit? {
-        lastWactedAt: items[items.length-1].lastWatchedAt.toISOString(),
-        _id: items[items.length-1]._id.toString(),
-    } : null;
-    
-    return { items, nextCursor };
-};
+  return WatchHistory.find(query)
+    .sort({ lastWatchedAt: -1, _id: -1 })
+    .limit(limit)
+    .lean();
+}
