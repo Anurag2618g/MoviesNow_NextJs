0cd87b146d060ca074f939d8b9cf2c46807eab37
Anurag2618g <anurag2618g@gmail.com>
2026-02-08 23:52:04 +0530

    add tmdb fetch by movie id

--- server/tmdb/movies.ts
+++ server/tmdb/movies.ts
@@ -1,6 +1,6 @@
 import { getCache, setCache } from "../cache/redisCache";
 import { tmdbFetch } from "./tmdb.client";
-import { TmdbListResponse, TmdbMovie } from "./types";
+import { MovieDetails, TmdbListResponse, TmdbMovie, TmdbMovieResponse } from "./types";
 
 export const getTrendingMovies = async () => {
     const cacheKey = 'tmdb:movies:trending';
@@ -13,4 +13,28 @@ export const getTrendingMovies = async () => {
 
     setCache(cacheKey, data, 60*60);
     return data;
+};
+
+export const getMovieById = async (tmdbId: number) => {
+    const cacheKey = `tmdb:movie:${tmdbId}`;
+    const cached = await getCache<MovieDetails>(cacheKey);
+    if (cached) {
+        return cached;
+    }
+
+    const data = await tmdbFetch<TmdbMovieResponse>(`movie/${tmdbId}`);
+
+    const movie: MovieDetails = {
+        id: data.id,
+        title: data.title,
+        overview: data.overview,
+        posterPath: data.poster_path,
+        backdropPath: data.backdrop_path,
+        releaseDate: data.release_date,
+        rating: data.vote_average,
+        voteCount: data.vote_count,
+    };
+
+    setCache(cacheKey, movie, 60 * 60 * 24);
+    return movie;
 };
