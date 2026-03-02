340655deb2caa68766d48c4708951ea64537e676
Anurag2618g <anurag2618g@gmail.com>
2026-03-02 23:54:11 +0530

    hot updates

--- server/watch/watch.model.ts
+++ server/watch/watch.model.ts
@@ -11,16 +11,16 @@ const watchHistorySchema = new Schema(
             type: String,
             required: true,
         },
-        contentSnapshot: {
-            title: {type: String, required: true},
-            posterPath: {type: String, default: null},
-            backdropPath: {type: String, default: null},
-            rating: {type: Number, required: true},
-            snapShotUpdateAt: {
-                type: Date,
-                required: true,
-            }
-        },
+        // contentSnapshot: {
+        //     title: {type: String, required: true},
+        //     posterPath: {type: String, default: null},
+        //     backdropPath: {type: String, default: null},
+        //     rating: {type: Number, required: true},
+        //     snapShotUpdateAt: {
+        //         type: Date,
+        //         required: true,
+        //     }
+        // },
         progress: {
             type: Number,
             required: true,
@@ -47,8 +47,6 @@ watchHistorySchema.index({ userId: 1, contentId: 1 }, { unique: true });
 watchHistorySchema.index({ userId: 1, status: 1,  lastWatchedAt: -1 });
 watchHistorySchema.index({ userId: 1, lastWatchedAt: -1, _id: -1 });
 
-export const WatchHistory = mongoose.models.WatchHistory || mongoose.model("WatchHistory", watchHistorySchema);
-
 const ContinueWatchingSchema = new Schema({
     userId: String,
     contentId: String,
@@ -59,4 +57,5 @@ const ContinueWatchingSchema = new Schema({
 
 ContinueWatchingSchema.index({ userId: 1, lastWatchedAt: -1 });
 
+export const WatchHistory = mongoose.models.WatchHistory || mongoose.model("WatchHistory", watchHistorySchema);
 export const ContinueWatching = mongoose.models.ContinueWatching || mongoose.model("ContinueWatching", ContinueWatchingSchema);
