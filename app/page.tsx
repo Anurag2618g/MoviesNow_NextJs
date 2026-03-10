4ab290ec88736cf0585d4570f32e78fdf6bc11bf
Anurag2618g <anurag2618g@gmail.com>
2026-03-10 23:56:22 +0530

    now the app is listening to events

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import "@/infrastructure/events/registerProjections";
+
 export default function Home() {
   return (
     <>
---
+++ infrastructure/events/registerProjections.ts
@@ -0,0 +1,1 @@
+import "@/server/continueWatching/continueWatching.projection";
