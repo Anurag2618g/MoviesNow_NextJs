f5e4e1376cf845802eb3bc70ce32438f73353bd2
Anurag2618g <anurag2618g@gmail.com>
2026-03-24 23:57:01 +0530

    added analytics schema

---
+++ server/analytics/analytics.model.ts
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from "mongoose";
+
+const analyticsSchema = new Schema({
+  userId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  totalWatchTime: {
+    type: Number,
+    default: 0,
+  },
+
+  totalCompleted: {
+    type: Number,
+    default: 0,
+  },
+
+  totalStarted: {
+    type: Number,
+    default: 0,
+  },
+
+  genreCounts: {
+    type: Map,
+    of: Number,
+    default: {},
+  }
+
+}, { timestamps: true });
+
+export const Analytics =
+  mongoose.models.Analytics ||
+  mongoose.model("Analytics", analyticsSchema);
