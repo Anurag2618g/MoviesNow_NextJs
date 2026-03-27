218de46562a86cf026bc6ac50593a7c02ceea8e4
Anurag2618g <anurag2618g@gmail.com>
2026-03-27 23:55:37 +0530

    updated model

--- server/analytics/analytics.model.ts
+++ server/analytics/analytics.model.ts
@@ -1,35 +1,28 @@
 import mongoose, { Schema } from "mongoose";
 
 const analyticsSchema = new Schema({
-  userId: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-
-  totalWatchTime: {
-    type: Number,
-    default: 0,
-  },
-
-  totalCompleted: {
-    type: Number,
-    default: 0,
-  },
-
-  totalStarted: {
-    type: Number,
-    default: 0,
-  },
-
-  genreCounts: {
-    type: Map,
-    of: Number,
-    default: {},
-  }
-
+    userId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    totalWatchTime: {
+        type: Number,
+        default: 0,
+    },
+    totalCompleted: {
+        type: Number,
+        default: 0,
+    },
+    totalStarted: {
+        type: Number,
+        default: 0,
+    },
+    genreCount: {
+        type: Map,
+        of: Number,
+        default: {},
+    }
 }, { timestamps: true });
 
-export const Analytics =
-  mongoose.models.Analytics ||
-  mongoose.model("Analytics", analyticsSchema);
+export const Analytics = mongoose.models.Analytics || mongoose.model('Analytics', analyticsSchema);
